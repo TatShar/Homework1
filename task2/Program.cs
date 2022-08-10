fcaa32c0988bc74b87eeb58dcd92d2ebe1367002
TatShar <tvku@yandex.ru>
2022-08-10 19:23:24 +0300

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее
+
+Console.Write("Ведите первое число ");
+int number1 = Convert.ToInt16(Console.ReadLine());
+Console.Write("Введите второе число ");
+int number2 = Convert.ToInt16(Console.ReadLine());
+
+if (number1 > number2)
+    {
+        int max = number1;
+        Console.WriteLine("Большее из двух чисел: " + max);
+    }
+else 
+{
+    int max = number2;
+    Console.WriteLine("Большее из двух чисел: " + max);
+}
